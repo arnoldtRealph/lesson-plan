f60b37af5f63a974738103c503c3780289eb0513
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-13 23:12:45 +0200

    Initial commit

--- plan.py
+++ plan.py
@@ -7,22 +7,21 @@ import docx
 st.set_page_config(page_title="Lesson Plan Creator", page_icon=":books:")
 
 # Create input fields
-st.title("HOЁRSKOOL SAUL DAMON")
-<<<<<<< HEAD
-st.subheader("LESBEPLANNER")
-st.write("Vul asseblief die volgende velde in om 'n nuwe lesplan te skep:")
+st.title("SAUL DAMON HIGH SCHOOL")
+st.subheader("LESSON PLANNER")
+st.write("Please fill in the following fields to create a new lesson plan:")
 st.write("")
-subject = st.text_input("Subject")
-lesson_title = st.text_input("Lesson Title")
-grade = st.selectbox("Grade", ["Grade 9", "Grade 10", "Grade 11", "Grade 12"])
-lesson_date = st.date_input("Lesson Date")
-lesson_objective = st.text_area("Lesson Objective")
-lesson_activities = st.text_area("Lesson Activities")
-materials_needed = st.text_area("Materials Needed")
-homework = st.text_area("Homework")
-notes = st.text_area("Notes")
-teacher_name = st.text_input("Teacher Name")
-teacher_surname = st.text_input("Teacher Surname")
+subject = st.text_input("SUBJECT")
+lesson_title = st.text_input("LESSON TITLE")
+grade = st.selectbox("GRADE", ["GRADE 9", "GRADE 10", "GRADE 11", "GRADE 12"])
+lesson_date = st.date_input("LESSON DATE")
+lesson_objective = st.text_area("LESSON OBJECTIVES")
+lesson_activities = st.text_area("LESSON ACTIVITIES")
+materials_needed = st.text_area("MATERIAL NEEDED")
+homework = st.text_area("HOMEWRK ACTIVITIES")
+notes = st.text_area("NOTES")
+teacher_name = st.text_input("INITIALS")
+teacher_surname = st.text_input("SURNAME")
 st.write("")
 st.write("Created by Mr. A.R Visagie @ Saul Damon High School")
 
@@ -33,67 +32,24 @@ if st.button("Create Lesson Plan"):
     # Add input values to the document
     document.add_heading(subject.upper(), level=0)
     document.add_paragraph("")
-    document.add_paragraph("Lesson Title: " + lesson_title)
-    document.add_paragraph("Grade: " + grade)
-    document.add_paragraph("Lesson Date: " + str(lesson_date))
+    document.add_paragraph("LESSON TITLE: " + lesson_title)
+    document.add_paragraph("GRADE: " + grade)
+    document.add_paragraph("LESSON DATE: " + str(lesson_date))
     document.add_paragraph("")
-    document.add_heading("Lesson Objective", level=1)
-=======
-st.header("LESBEPLANNER")
-# Define the grade levels
-grade_levels = ['GRAAD 9', 'GRAAD 10', 'GRAAD 11', 'GRAAD 12']
-
-# Set up the sidebar
-st.sidebar.title("DAAGLIKE BEPLANNER")
-selected_grade = st.sidebar.selectbox("KIES N GRAAD", grade_levels)
-
-# Define the lesson plan inputs
-subject_name = st.text_input("VAK")
-lesson_title = st.text_input("LES TITEL")
-lesson_date = st.date_input("LES DATUM")
-lesson_objective = st.text_input("LES DOELWIT")
-lesson_activities = st.text_area("LEERDER AKTIWITEITE")
-lesson_materials = st.text_area("MATERIAAL BENODIG")
-lesson_homework = st.text_area("HUISWERK")
-lesson_notes = st.text_area("NOTAS")
-
-st.write("Designed by Mr. A.R Visagie @ Saul Damon High School")
-# Set up the save button
-if st.button("Save"):
-    # Check if the file already exists
-    file_path = os.path.expanduser(f"~/Desktop/{lesson_title}.docx")
-    while os.path.exists(file_path):
-        st.warning(f"A file with the name '{lesson_title}.docx' already exists on your desktop. Please choose a different name.")
-        lesson_title = st.text_input("Lesson Title")
-        file_path = os.path.expanduser(f"~/Desktop/{lesson_title}.docx")
-        
-    # Add the lesson plan to the document
-    p = document.add_paragraph(selected_grade, style='Heading 1')
-    p = document.add_paragraph()
-    p.add_run(f"Subject: ").bold = True
-    p.add_run(f"{subject_name}").bold = True
-    p = document.add_paragraph(lesson_title, style='Heading 1')
-    
-    # Add the lesson date
-    date_str = lesson_date.strftime('%A, %B %d, %Y')
-    document.add_paragraph(date_str, style='Heading 2')
-    
-    # Add the lesson objective, activities, materials, and homework
-    document.add_heading("Objective", level=2)
->>>>>>> a594bd09b9a9a4275a9a4ef2152a34007de208e9
+    document.add_heading("LESSON OBJECTIVES", level=1)
     document.add_paragraph(lesson_objective)
-    document.add_heading("Lesson Activities", level=1)
+    document.add_heading("LESSON ACTIVITIES", level=1)
     document.add_paragraph(lesson_activities)
-    document.add_heading("Materials Needed", level=1)
+    document.add_heading("MATERIAL NEEDED", level=1)
     document.add_paragraph(materials_needed)
-    document.add_heading("Homework", level=1)
+    document.add_heading("HOMEWRK ACTIVITIES", level=1)
     document.add_paragraph(homework)
-    document.add_heading("Notes", level=1)
+    document.add_heading("NOTES", level=1)
     document.add_paragraph(notes)
     document.add_paragraph("")
-    document.add_paragraph("Teacher Name: " + teacher_name)
-    document.add_paragraph("Teacher Surname: " + teacher_surname)
-    document.add_paragraph("Teacher Signature: ")
+    document.add_paragraph("INITIALS: " + teacher_name)
+    document.add_paragraph("SURNAME: " + teacher_surname)
+    document.add_paragraph("SIGNATURE: ")
 
     # Save document to BytesIO object
     with io.BytesIO() as output:
